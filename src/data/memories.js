cf74d29b39db421afffc648f09dd969066cdc3f2
yethiha2000 <yairthiha1001@gmail.com>
2026-08-24 16:45:23 +0630

    updated image and text

--- src/data/memories.js
+++ src/data/memories.js
@@ -5,4 +5,9 @@ export const memories = [
   { id: 4, image: "/images/memory-04.jpg", },
   { id: 5, image: "/images/memory-05.jpg",  },
   { id: 6, image: "/images/memory-06.jpg", },
+  { id: 7, image: "/images/memory-07.jpg", },
+  { id: 8, image: "/images/memory-08.jpg", },
+  { id: 9, image: "/images/memory-09.jpg", },
+  { id: 10, image: "/images/memory-010.jpg", },
+  { id: 11, image: "/images/memory-011.jpeg", },
 ];
